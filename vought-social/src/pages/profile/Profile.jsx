1d9fcfe3985c0a9c47e8776a0f401d2d28fb47a4
vader2k <76834964+vader2k@users.noreply.github.com>
2023-09-23 16:48:12 +0100

    text settings

--- vought-social/src/pages/profile/Profile.jsx
+++ vought-social/src/pages/profile/Profile.jsx
@@ -7,13 +7,13 @@ import {Post} from '../../components'
 const Profile = () => {
   return (
     <div>
-      <div className='flex items-center justify-center flex-col relative w-[100%] overflow-hidden'>
+      <div className='flex items-center justify-center flex-col relative w-[100%] overflow-hidden dark:text-white'>
         <img src={cover1} className='sm:h-[400px] object-fill sm:w-[100%] w-[800px]'/>
         <img src={onyedika} className='sm:w-[200px] w-[100px] sm:h-[200px] h-[100px] object-cover rounded-full absolute sm:top-[300px] top-[250px]'/>
         <p className='sm:mt-[100px] mt-[60px] font-semibold sm:text-[1.5rem] text-[1rem]'>Onyedika Chigelu</p>
       </div>
 
-      <div className='sm:py-[20px] sm:px-[70px] py-[10px] px-[20px]'>
+      <div className='sm:py-[20px] sm:px-[70px] py-[10px] px-[20px] dark:text-white'>
         <div className='flex items-center justify-evenly mt-[20px] gap-14 shadow py-5 px-5 mr-3 sm:mr-0'>
             <div className='flex-1 gap-3 items-center justify-center sm:flex hidden'>
               <FaFacebook className='sm:text-[25px]  text-gray-600'/> <FaInstagram  className='text-[25px] text-gray-600'/> <AiOutlineTwitter className='text-[25px] text-gray-600'/> <FaLinkedin  className='text-[25px] text-gray-600'/> <FaPinterest  className='text-[25px] text-gray-600'/>
